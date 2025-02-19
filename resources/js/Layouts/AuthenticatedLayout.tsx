28aacda07c042a8962d1234d4427b1b49c6440c5
Muhammad Nurfatkhur Rahman <mhmd.nurfat@gmail.com>
2025-02-19 22:22:11 +0700

    Refactor sidebar and navigation components for improved structure and functionality

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,7 +1,7 @@
 import Loader from '@/Components/Loader';
 import { navItems } from '@/lib/navItems';
 import { usePage } from '@inertiajs/react';
-import { PropsWithChildren, useState } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import Header from './Header/Header';
 import { Navbar } from './Navbar/Navbar';
@@ -16,9 +16,37 @@ export default function AuthenticatedLayout({
     processing = false,
 }: PropsWithChildren<AuthenticatedLayout>) {
     const user = usePage().props.auth.user;
+    const currentPath = usePage().url;
 
-    const [sideItems, setSideItems] = useState<string[]>([]);
+    const [sideItems, setSideItems] = useState<
+        Array<{ name: string; route: string }>
+    >([]);
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+    const findSidebarItemsByPath = () => {
+        for (const navItem of navItems) {
+            if (navItem.dropdownItems) {
+                for (const dropdownItem of navItem.dropdownItems) {
+                    if (dropdownItem.sidebarItems) {
+                        const matchingSidebarItem =
+                            dropdownItem.sidebarItems.find((item) => {
+                                const itemUrl = new URL(item.route);
+
+                                return itemUrl.pathname === currentPath;
+                            });
+                        if (matchingSidebarItem) {
+                            return dropdownItem.sidebarItems;
+                        }
+                    }
+                }
+            }
+        }
+        return [];
+    };
+
+    useEffect(() => {
+        setSideItems(findSidebarItemsByPath());
+    }, [currentPath]);
 
     const handleNavItemClick = (itemName: string, dropdownName?: string) => {
         if (dropdownName) {
@@ -37,11 +65,7 @@ export default function AuthenticatedLayout({
             <ToastContainer />
             <div className="min-h-screen">
                 <div className="relative flex min-h-screen max-w-full dark:bg-black">
-                    <Sidebar
-                        sideItems={sideItems}
-                        isOpen={isSidebarOpen}
-                        href={'#'}
-                    />
+                    <Sidebar sideItems={sideItems} isOpen={isSidebarOpen} />
                     <div className="relative flex flex-1 flex-col overflow-x-hidden">
                         <Header
                             user={user.name}
--- resources/js/lib/navItems.ts
+++ resources/js/lib/navItems.ts
@@ -11,13 +11,14 @@ import {
 
 interface DropdownItem {
     name: string;
-    link: string;
-    sidebarItems?: string[];
+    sidebarItems?: Array<{
+        name: string;
+        route: string;
+    }>;
 }
 
 interface NavItem {
     name: string;
-    link: string;
     icon?: IconType;
     dropdownItems?: DropdownItem[];
 }
@@ -25,110 +26,59 @@ interface NavItem {
 export const navItems: NavItem[] = [
     {
         name: 'PMB',
-        link: '/pmb',
         icon: FaUserGraduate,
-        dropdownItems: [
-            {
-                name: 'Pendaftaran',
-                link: '/pmb/pendaftaran',
-                sidebarItems: [
-                    'Form Pendaftaran Baru',
-                    'Upload Berkas',
-                    'Cetak Formulir',
-                    'Jadwal Ujian',
-                    'Pembayaran Pendaftaran',
-                ],
-            },
-            {
-                name: 'Status',
-                link: '/pmb/status',
-                sidebarItems: [
-                    'Status Pendaftaran',
-                    'Hasil Seleksi',
-                    'Pengumuman Kelulusan',
-                    'Daftar Ulang',
-                ],
-            },
-        ],
     },
     {
         name: 'Data Master',
-        link: '/data-master',
         icon: FaDatabase,
         dropdownItems: [
             {
                 name: 'Mahasiswa',
-                link: '/data-master/mahasiswa',
-                sidebarItems: [
-                    'Data Pribadi',
-                    'Riwayat Akademik',
-                    'Dokumen Mahasiswa',
-                    'Status Aktif',
-                ],
             },
             {
                 name: 'Dosen',
-                link: '/data-master/dosen',
-                sidebarItems: [
-                    'Data Pribadi Dosen',
-                    'Riwayat Mengajar',
-                    'Penelitian',
-                    'Jadwal Mengajar',
-                ],
             },
             {
                 name: 'Mata Kuliah',
-                link: '/data-master/mata-kuliah',
-                sidebarItems: [
-                    'Daftar Mata Kuliah',
-                    'Silabus',
-                    'Materi Kuliah',
-                    'Jadwal Kuliah',
-                ],
             },
         ],
     },
     {
         name: 'Akademik',
-        link: '/akademik',
         icon: FaGraduationCap,
-        dropdownItems: [
-            { name: 'Jadwal Kuliah', link: '/akademik/jadwal-kuliah' },
-            { name: 'Nilai', link: '/akademik/nilai' },
-        ],
     },
     {
         name: 'Kemahasiswaan',
-        link: '/kemahasiswaan',
         icon: FaUsers,
-        dropdownItems: [
-            { name: 'Aktivitas', link: '/kemahasiswaan/aktivitas' },
-            { name: 'Organisasi', link: '/kemahasiswaan/organisasi' },
-        ],
     },
     {
         name: 'Feeder',
-        link: '/feeder',
         icon: FaSync,
-        dropdownItems: [
-            { name: 'Jadwal Kuliah', link: '/feeder/jadwal-kuliah' },
-            { name: 'Nilai', link: '/feeder/nilai' },
-        ],
     },
     {
         name: 'Keuangan',
-        link: '/keuangan',
         icon: FaMoneyBillWave,
+        dropdownItems: [
+            {
+                name: 'Pembayaran',
+            },
+            {
+                name: 'Pembayaran',
+            },
+        ],
     },
     {
         name: 'Settings',
-        link: '/settings',
         icon: FaCog,
         dropdownItems: [
             {
                 name: 'Hak Akses',
-                link: '/settings/hak-akses',
-                sidebarItems: ['Hak Akses', 'User'],
+                sidebarItems: [
+                    {
+                        name: 'Hak Akses',
+                        route: route('system.permissions.index'),
+                    },
+                ],
             },
         ],
     },
